4c45fa9f13c8d4f46683ba6cbdc56b8d478b342c
julfed7 <87260594+julfed7@users.noreply.github.com>
2023-09-16 03:53:44 +0900

    Update client.py
    [+] multiplayer
    [+] possibility to leave the lobby

--- client.py
+++ client.py
@@ -7,8 +7,16 @@ import time
 import random
 from pygame.math import Vector2
 
+current_scene = 0
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 2222))
+
+try:
+    s.connect(("localhost", 2222))
+except:
+    current_scene = 3
+finally:
+    print("Client running. Version 1.0")
 
 client_id = None
 
@@ -17,6 +25,29 @@ response_queue = []
 request_queue = []
 
 failed_disconnect_attempts = 0
+
+camera_offset_x = 0
+camera_offset_y = 0
+camera_offset_z = 0
+camera_locked = True
+
+delta = 0
+
+t1 = time.time()
+
+def delta_update():
+    global delta, t1
+    t2 = time.time()
+    delta = t2-t1
+    t1 = t2
+
+def remove_object(id):
+    for object in game_renderer.scenes[game_renderer.current_scene].objects:
+        if object.id == id:
+            game_renderer.scenes[game_renderer.current_scene].objects.remove(object)
+
+def add_object(object):
+    game_renderer.scenes[game_renderer.current_scene].objects.append(object)   
 
 def create_response(message=None, data=None, id=client_id):
     response = {
@@ -73,21 +104,55 @@ def response_get_online(data):
     return True
 
 def response_hub(data):
+    print("switched to hub scene")
     game_renderer.set_scene(1)
     return True
 
 def response_game(data):
+    print("switched to game scene")
     game_renderer.set_scene(2)
     response = create_response("joined", None, client_id)
     request_sender(response)
     return True
+
+def response_get_objects(data):
+    global game
+    for object in game.objects:
+        isIn = False
+        for game_object in data["data"]:
+            if game_object["id"] == object.id:
+                isIn = True
+        if not isIn:
+            remove_object(object.id)
+    for game_object in data["data"]:
+        isIn = False
+        for object in game.objects:
+            if object.id == game_object["id"]:
+                isIn = True
+        if not isIn:
+            player = Player(game_object["id"], game_object["x"], game_object["y"])
+            player.set_scale(5, 5)
+            add_object(player)
+    return True
+
+def response_get_error_disconnect_game_started(data):
+    print("switched to error disconnect game started scene")
+    game_renderer.set_scene(4)
+    return True
+
+def response_get_access_to_leave_hub(data):
+    print("switched to menu scene")
+    game_renderer.set_scene(0)
 
 messages = {
     "response": {
         "get_id": response_get_id,
         "get_access_to_exit": response_exit,
         "get_online": response_get_online,
-        "get_access_to_hub": response_hub
+        "get_access_to_hub": response_hub,
+        "get_objects": response_get_objects,
+        "get_error_disconnect_game_started": response_get_error_disconnect_game_started,
+        "get_access_to_leave_hub": response_get_access_to_leave_hub
     },
     "request": {
         "ping": response_ping,
@@ -120,22 +185,35 @@ def response_handler():
         time.sleep(0.0166666666666667)
                 
 def request_handler():
+    error_count = 0
     print("\nworker: request handler started", end="")
     while True:
         if request_queue:
             for message in request_queue:
-                s.send(message)
-                request_queue.remove(message)
-                print("sended", message)
+                try:
+                    s.send(message)
+                    request_queue.remove(message)
+                    print("sended", message)
+                except OSError:
+                    error_count += 1
+                    if error_count <= 1:
+                        print("error. Request can't be sended")
         time.sleep(0.0166666666666667)
 
 class Scene:
     def __init__(self):
         self.objects = []
+        self.ticks = 0
+    def setup(self):
+        pass
     def tick(self):
+        self.ticks += 1
+        if self.ticks == 1:
+            self.setup()
         for object in self.objects:
-            object.draw()
-            object.update()
+            image = pygame.transform.scale(object.image, (object.rect.width+camera_offset_z, object.rect.height+camera_offset_z))
+            game_renderer.screen.blit(image, (object.rect.x+camera_offset_x, object.rect.y+camera_offset_y))
+            object.tick()
 
 class GameRenderer:
     def __init__(self, screen):
@@ -160,22 +238,29 @@ class GameRenderer:
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
 game_renderer = GameRenderer(screen)
 
 class Object:
-    image_button_play = pygame.image.load("button_play.png").convert_alpha()
-    image_menu_start = pygame.image.load("menu_start.png").convert_alpha()
-    image_menu_title = pygame.image.load("menu_title.png").convert_alpha()
-    font_super_text = pygame.font.Font("Visitor Rus.ttf", 50)
-    def __init__(self, image, x, y):
-        self.position = Vector2(x, y)
+    image_menu_gui_button_play = pygame.image.load("button_play.png").convert_alpha()
+    image_menu_gui_bg_start = pygame.image.load("menu_start.png").convert_alpha()
+    image_menu_spr_title = pygame.image.load("menu_title.png").convert_alpha()
+    image_game_spr_churka = pygame.image.load("churka.png").convert_alpha()
+    font_super_text = pygame.font.Font("hub.ttf", 50)
+    font_small_text = pygame.font.Font("hub.ttf", 25)
+    def __init__(self, id, image, x, y):
+        self.id = id
         self.image = image
+        self.position = Vector2(x, y)
         self.rect = self.image.get_rect(center=self.position)
-    def draw(self):
-        game_renderer.screen.blit(self.image, self.position)
-    def update(self):
-        self.rect.center = self.position
+        self.ticks = 0
+    def setup(self):
+        pass
+    def tick(self):
+        self.ticks += 1
+        if self.ticks == 1:
+            self.setup()
+        self.image.get_rect(center=self.position)
     def set_scale(self, width=1, height=1):
         self.image = pygame.transform.scale(self.image, (self.image.get_width()*width, self.image.get_height()*height))
         self.rect = self.image.get_rect(center=self.position)
@@ -184,12 +269,10 @@ class Object:
 
 class ButtonPlay(Object):
     def __init__(self, x, y):
-        super().__init__(Object.image_button_play, x, y)
+        super().__init__(random.randint(10001, 20001), Object.image_menu_gui_button_play, x, y)
         self.isPressed = False
-    def draw(self):
-        super().draw()
-    def update(self):
-        super().update()
+    def tick(self):
+        super().tick()
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             mouseButton = pygame.mouse.get_pressed()
             if mouseButton[0] and not self.isPressed:
@@ -206,48 +289,71 @@ class ButtonPlay(Object):
 
 class MenuStart(Object):
     def __init__(self, x, y):
-        super().__init__(Object.image_menu_start, x, y)
-    def draw(self):
-        super().draw()
-    def update(self):
-        super().update()
+        super().__init__(random.randint(10001, 20001), Object.image_menu_gui_bg_start, x, y)
+    def tick(self):
+        super().tick()
 
 class MenuTitle(Object):
     def __init__(self, x, y):
-        super().__init__(Object.image_menu_title, x, y)
-    def draw(self):
-        super().draw()
-    def update(self):
-        super().update()
+        super().__init__(random.randint(10001, 20001), Object.image_menu_spr_title, x, y)
 
 class HubOnlineViewer(Object):
     def __init__(self, x, y):
-        self.position = Vector2(x, y)
-        self.image = Object.font_super_text
+        self.font = Object.font_super_text
         self.online = 0
-        self.text = self.image.render(f"Hub ONLINE: {self.online} / 2", False, (255, 255, 255))
-        self.rect = self.text.get_rect(center=self.position)
+        self.image = self.font.render(f"Hub ONLINE: {self.online} / 2", False, (255, 255, 255))
+        self.rect = self.image.get_rect(center=(x, y))
+        self.position = Vector2(x, y)
         self.start_time = time.time()
-    def draw(self):
-        game_renderer.screen.blit(self.text, self.position)
-    def update(self):
-        super().update()
+        super().__init__(random.randint(10001, 20001), self.image, self.position[0], self.position[1])
+    def tick(self):
+        super().tick()
         if time.time()-self.start_time>=3:
-            self.text = self.image.render(f"Hub ONLINE: {self.online} / 2", False, (255, 255, 255))
+            self.image = self.font.render(f"Hub ONLINE: {self.online} / 2", False, (255, 255, 255))
             request = create_request("online", None, client_id)
             request_sender(request)
             self.start_time = time.time()
     def set_online(self, value):
         self.online = value
 
+class HubEscapeText(Object):
+    def __init__(self, x, y):
+        self.font = Object.font_small_text
+        self.image = self.font.render(f"ESC BUTTON - EXIT", False, (255, 255, 255))
+        self.rect = self.image.get_rect(center=(x, y))
+        self.position = Vector2(x, y)
+        super().__init__(random.randint(10001, 20001), self.image, self.position[0], self.position[1])
+
+class ErrorDisconnectText(Object):
+    def __init__(self, x, y):
+        self.font = Object.font_super_text
+        self.online = 0
+        self.image = self.font.render(f":(", False, (255, 255, 255))
+        self.rect = self.image.get_rect(center=(x, y))
+        self.position = Vector2(x, y)
+        super().__init__(random.randint(10001, 20001), self.image, self.position[0], self.position[1])
+
+class ErrorDisconnectGameStartedText(Object):
+    def __init__(self, x, y):
+        self.font = Object.font_super_text
+        self.online = 0
+        self.image = self.font.render(f"Game started. Try again later. :)", False, (255, 255, 255))
+        self.rect = self.image.get_rect(center=(x, y))
+        self.position = Vector2(x, y)
+        super().__init__(random.randint(10001, 20001), self.image, self.position[0], self.position[1])
+
+class Player(Object):
+    def __init__(self, id, x, y):
+        super().__init__(id, Object.image_game_spr_churka, x, y)
+
 class Menu(Scene):
     def __init__(self):
         super().__init__()
-        menu_start = MenuStart(0, 0)
+        menu_start = MenuStart(640, 360)
         menu_start.set_scale(80, 80)
-        menu_title = MenuTitle(468, 130)
+        menu_title = MenuTitle(640, 150)
         menu_title.set_scale(8, 8)
-        button_play = ButtonPlay(512, 280)
+        button_play = ButtonPlay(640, 360)
         button_play.set_scale(8, 8)
         self.objects.append(menu_start)
         self.objects.append(menu_title)
@@ -259,8 +365,10 @@ class Menu(Scene):
 class Hub(Scene):
     def __init__(self):
         super().__init__()
-        hub_online_viewer = HubOnlineViewer(300, 100)
+        hub_online_viewer = HubOnlineViewer(640, 100)
+        hub_escape_text = HubEscapeText(120, 700)
         self.objects.append(hub_online_viewer)
+        self.objects.append(hub_escape_text)
     def tick(self):
         game_renderer.screen.fill((0, 0, 0))
         super().tick()
@@ -268,9 +376,41 @@ class Hub(Scene):
 class Game(Scene):
     def __init__(self):
         super().__init__()
+        self.last_ticks = 0
     def tick(self):
         game_renderer.screen.fill((125, 125, 125))
         super().tick()
+        if self.ticks-self.last_ticks>16:
+            self.get_objects()
+            self.last_ticks = self.ticks
+    def get_objects(self):
+        request = create_request("objects", None, client_id)
+        request_sender(request)
+
+class ErrorDisconnect(Scene):
+    def __init__(self):
+        super().__init__()
+        error_disconnect_text = ErrorDisconnectText(640, 360)
+        self.objects.append(error_disconnect_text)
+    def setup(self):
+        print("Error. Can't connect to the server")
+    def tick(self):
+        game_renderer.screen.fill((0, 0, 0))
+        super().tick()
+
+class ErrorDisconnectGameStarted(Scene):
+    def __init__(self):
+        super().__init__()
+        error_disconnect_game_started_text = ErrorDisconnectGameStartedText(640, 360)
+        self.objects.append(error_disconnect_game_started_text)
+    def setup(self):
+        print("Error. Can't connect to the server. Game started.")
+    def tick(self):
+        game_renderer.screen.fill((0, 0, 0))
+        super().tick()
+        if True in pygame.mouse.get_pressed():
+            print("switched to menu scene")
+            game_renderer.set_scene(0)
 
 def main():
     global screen, clock, fps, running, menu, hub, game
@@ -280,9 +420,14 @@ def main():
     menu = Menu()
     hub = Hub()
     game = Game()
+    error_disconnect = ErrorDisconnect()
+    error_disconnect_game_started = ErrorDisconnectGameStarted()
     game_renderer.add_scene(menu)
     game_renderer.add_scene(hub)
     game_renderer.add_scene(game)
+    game_renderer.add_scene(error_disconnect)
+    game_renderer.add_scene(error_disconnect_game_started)
+    game_renderer.set_scene(current_scene)
     print("workers starting...")
     threads0 = []
     thread0 = threading.Thread(target=data_handler)
@@ -303,13 +448,29 @@ if __name__ == "__main__":
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            failed_disconnect_attempts += 1
+            if failed_disconnect_attempts >= 5:
+                print("emergency exit")
+                running = False
             request = create_request("disconnect", id=client_id)
             request_sender(request)
-            failed_disconnect_attempts += 1
-            if failed_disconnect_attempts >= 10:
-                running = False
+        if event.type == pygame.MOUSEWHEEL:
+            if event.y == 1 and not camera_locked:
+                camera_offset_z += 1
+            elif event.y == -1 and not camera_locked:
+                camera_offset_z -= 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_TAB and not camera_locked:
+                camera_offset_x = 0
+                camera_offset_y = 0
+                camera_offset_z = 0
+            elif event.key == pygame.K_ESCAPE and game_renderer.current_scene == 1:
+                request = create_request("leave_hub", None, client_id)
+                request_sender(request)
+                
     game_renderer.tick()
     pygame.display.flip()
+    delta_update()
     clock.tick(fps)
 s.close()
 pygame.quit()
