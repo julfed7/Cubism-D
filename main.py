bd0fd61d67412249af99ebc67981681dd5a1e71a
julfed7 <87260594+julfed7@users.noreply.github.com>
2024-12-26 23:46:32 +0000

    Automatic commit before Assistant edits

--- main.py
+++ main.py
@@ -5,77 +5,89 @@ import pygame
 import time
 
 def resource_path(relative):
-	if hasattr(sys, "_MEIPASS"):
-		return os.path.join(sys._MEIPASS, relative)
-	return os.path.join(relative)
-	
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative)
+    return os.path.join(relative)
+    
 def path(this_path, environtment_os="android"):
-	if environtment_os == "windows":
-		return resource_path(this_path)
-	else:
-		return this_path
+    if environtment_os == "windows":
+        return resource_path(this_path)
+    else:
+        return this_path
 
 def read_file_json(path):
-	with open(path) as file:
-		data = file.read()
-		book = json.loads(data)
-	return book
-	
+    with open(path) as file:
+        data = file.read()
+        book = json.loads(data)
+    return book
+    
 def read_file(path):
-	if "/" in path:
-		file_name = path.split("/")[-1]
-	else:
-		file_name = path.split("\\")[-1]
-	file_type = file_name.split(".")[-1]
-	if file_type == "json":
-		return read_file_json(path)
+    if "/" in path:
+        file_name = path.split("/")[-1]
+    else:
+        file_name = path.split("\\")[-1]
+    file_type = file_name.split(".")[-1]
+    if file_type == "json":
+        return read_file_json(path)
 
 class Image:
-	def __init__(self, path):
-		self.path = path
-		self.surface = None
-	def setup(self):
-		self.set_surface(pygame.image.load(path(self.get_path())))
-	def get_path(self):
-		return self.path
-	def set_surface(self, new_surface):
-		self.surface = new_surface
-	def get_surface(self):
-		return self.surface
+    def __init__(self, path):
+        self.path = path
+        self.surface = None
+    def setup(self):
+        self.set_surface(pygame.image.load(path(self.get_path())))
+    def get_path(self):
+        return self.path
+    def set_surface(self, new_surface):
+        self.surface = new_surface
+    def get_surface(self):
+        return self.surface
 
 class Window:
-	def __init__(self, width, height):
-		self.width = width
-		self.height = height
-		self.title = "default"
-		self.surface = None
-		self.icon_image = None
-	def setup(self):
-		pygame.display.set_mode((self.get_width(), self.get_height()))
-		self.set_surface(pygame.display.get_surface())
-	def update(self):
-		pygame.display.set_mode((self.get_width(), self.get_height()))
-		pygame.display.set_caption(self.get_title())
-		pygame.display.set_icon(self.get_icon_image().get_surface())
-	def set_surface(self, new_surface):
-		self.surface = new_surface
-	def get_surface(self):
-		return self.surface
-	def get_width(self):
-		return self.width
-	def get_height(self):
-		return self.height
-	def set_title(self, new_title):
-		self.title = new_title
-	def get_title(self):
-		return self.title
-	def set_icon_image(self, new_image):
-		self.icon_image = new_image
-	def get_icon_image(self):
-		return self.icon_image
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.title = "default"
+        self.surface = None
+        self.icon_image = None
+        self.virtual_surface = None
+    def setup(self):
+        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.set_surface(pygame.display.get_surface())
+        self.set_virtual_surface(pygame.Surface((self.get_width(), self.get_height())))
+    def update(self):
+        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption(self.get_title())
+        pygame.display.set_icon(self.get_icon_image().get_surface())
+    def draw(self, image, position):
+        self.get_virtual_surface().blit(image.get_surface(), (position[0], position[1]))
+    def tick(self):
+        scaled_virtual_surface = pygame.transform.scale(self.get_virtual_surface(), pygame.display.get_surface().get_size())
+        self.get_surface().blit(scaled_virtual_surface, (0, 0))
+        pygame.display.flip()
+    def set_surface(self, new_surface):
+        self.surface = new_surface
+    def get_surface(self):
+        return self.surface
+    def get_width(self):
+        return self.width
+    def get_height(self):
+        return self.height
+    def set_title(self, new_title):
+        self.title = new_title
+    def get_title(self):
+        return self.title
+    def set_icon_image(self, new_image):
+        self.icon_image = new_image
+    def get_icon_image(self):
+        return self.icon_image
+    def set_virtual_surface(self, new_surface):
+        self.virtual_surface = new_surface
+    def get_virtual_surface(self):
+        return self.virtual_surface
 
 environtment_os = "android"		
-		
+        
 config = read_file(path("settings/app.json"))
 
 TITLE = config["Title"]
@@ -106,12 +118,14 @@ window.set_icon_image(icon_image)
 
 window.update()
 
+print(pygame.display.Info())
+
 while is_running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			is_running = False
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_ESCAPE:
-				is_running = False
-	pygame.display.flip()				
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                is_running = False
+    window.tick()			
 pygame.quit()
