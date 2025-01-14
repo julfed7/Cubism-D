dec79786054dd4bee1b1017b9b4e4ad3ad9654c3
julfed7 <87260594+julfed7@users.noreply.github.com>
2025-01-14 21:18:14 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,13 +1,15 @@
 class Game:
     def __init__(self):
-        self.__scenes = []
+        self.__scenes = {}
         self.__current_scene = 0
     def tick(self):
         self.get_scene().tick()
     def get_scene(self):
-        return self.scenes[self.current_scene]
+        return list(self.scenes.values())[self.current_scene]
     def add_scene(self, new_scene):
-        self.scenes.append(new_scene)
+        self.scenes.update({new_scene.name:new_scene})
+    def del_scene(self, scene_name):
+        self.scenes.pop(scene_name)
     @property
     def scenes(self):
         return self.__scenes
@@ -18,24 +20,15 @@ class Game:
     
 
 class Scene:
-    def __init__(self):
-        self.__game_objects = []
+    def __init__(self, name):
+        self.name = name
+        self.game_objects = []
     def tick(self):
         for game_object in self.game_objects:
             game_object.tick()
-    def add_game_object(self, new_game_object):
-        self.game_objects.append(new_game_object)
-    def del_game_object(self, game_object_or_index):
-        if isinstance(game_object_or_index, GameObject):
-            self.game_objects.remove(game_object_or_index)
-        else:
-            self.game_objects.remove(self.game_objects[game_object_or_index])
-    @property
-    def game_objects(self):
-        return self.__game_objects
 
-class GameObject:
-    def __init__(self):
-        pass
-    def tick(self):
-        pass
+
+class Grass:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
