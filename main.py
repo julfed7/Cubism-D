e835379599582f5b3a26e640d7a30f7f4817f8a8
julfed7 <87260594+julfed7@users.noreply.github.com>
2025-01-14 21:17:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,7 @@
-import os
-import sys
-import json
-import pygame
 import time
+import utils
 import logic
-
-def resource_path(relative):
-    if hasattr(sys, "_MEIPASS"):
-        return os.path.join(sys._MEIPASS, relative)
-    return os.path.join(relative)
-    
-def path(this_path, environtment_os="android"):
-    if environtment_os == "windows":
-        return resource_path(this_path)
-    else:
-        return this_path
-
-def read_file_json(path):
-    with open(path) as file:
-        data = file.read()
-        book = json.loads(data)
-    return book
-    
-def read_file(path):
-    if "/" in path:
-        file_name = path.split("/")[-1]
-    else:
-        file_name = path.split("\\")[-1]
-    file_type = file_name.split(".")[-1]
-    if file_type == "json":
-        return read_file_json(path)
-
-def get_events():
-    return [Event(event.type, event.key if hasattr(event, "key") else None) for event in pygame.event.get()]
-
-class Image:
-    def __init__(self):
-        self.surface = None
-
-class Window:
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.__virtual_surface = None
-        self.icon_image = None
-        self.surface = None
-        self.title = "pygame window"
-    def setup(self):
-        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.surface = pygame.display.get_surface()
-        self.virtual_surface = pygame.Surface((self.width, self.height))
-    def update(self):
-        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        pygame.display.set_caption(self.title)
-        pygame.display.set_icon(self.icon_image.surface)
-    def draw(self, image, position):
-        self.virtual_surface.blit(image.surface, (position[0], position[1]))
-    def tick(self):
-        window_size = self.surface.get_size()
-        size = (window_size[1]/9*16, window_size[1])
-        position = (window_size[0]/2-size[0]/2, window_size[1]/2-size[1]/2)
-        scaled_virtual_surface = pygame.transform.scale(self.virtual_surface, size)
-        self.surface.blit(scaled_virtual_surface, position)
-        pygame.display.flip()
-    def fill(self, color):
-        self.virtual_surface.fill(color)
-    @property
-    def width(self):
-        return self.__width
-    @property
-    def height(self):
-        return self.__height
-    @property
-    def virtual_surface(self):
-        return self.__virtual_surface
-    @virtual_surface.setter
-    def virtual_surface(self, new_surface):
-        self.__virtual_surface = new_surface
-
-class Event:
-    def __init__(self, type, key):
-        self.type = type
-        self.key = key
+import pygame
 
 ticks = 0
 
@@ -90,9 +10,9 @@ delta_time = last_time/1000
 
 is_running = True
 
-environtment_os = "android"		
+ENVIRONMENT_OS = "Android"		
         
-config = read_file(path("settings/app.json"))
+config = utils.read_file(utils.path("settings/app.json"))
 
 TITLE = config["Title"]
 
@@ -102,48 +22,101 @@ WIDTH = config["Width"]
 
 HEIGHT = config["Height"]
 
-icon_file_name = config["Icon_file_name"]
+PROPORTION_X = config["Proportion_x"]
+
+PROPORTION_Y = config["Proportion_y"]
+
+ICON_FILE_NAME = config["Icon_file_name"]
+
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+virtual_screen = pygame.Surface((WIDTH, HEIGHT))
+
+icon_image = pygame.image.load(utils.path("sprites/"+ICON_FILE_NAME))
+
+pygame.display.set_caption(TITLE)
+
+pygame.display.set_icon(icon_image)
+
+
+sprites = {
+    logic.Grass: pygame.image.load(utils.path("sprites/grass.png"))
+}
+
 
 game = logic.Game()
 
-scene = logic.Scene()
+scenes = utils.read_file(utils.path("settings/scenes.json"))
 
-game_object = logic.GameObject()
+for scene_name in scenes:
+    scene_parameters = scenes[scene_name]
+    
+    new_scene = logic.Scene(scene_name)
+    
+    for game_object_name in scene_parameters["Game_objects"]:
+        game_object_parameters = scene_parameters["Game_objects"][game_object_name]
+        
+        arguments = {game_object_parameter_name.lower():game_object_parameter_value for game_object_parameter_name, game_object_parameter_value in game_object_parameters.items()}
 
-scene.add_game_object(game_object)
+        removing_arguments_name = []
+        
+        for argument_name in arguments:
+            if argument_name == "type":
+                removing_arguments_name.append(argument_name)
 
-pygame.init()
+        for removing_argument_name in removing_arguments_name:
+            arguments.pop(removing_bargument_name)
 
-window = Window(WIDTH, HEIGHT)
-window.setup()
+        if game_object_parameters["Type"] == "Grass":
+            game_object = logic.Grass(game_object_name, **arguments)
+        
+        new_scene.game_objects.append(game_object)
+ 
+    game.add_scene(new_scene)
 
-screen = window.surface
 
-window.title = TITLE
-
-icon_image = Image()
-icon_image.surface = pygame.image.load("sprites/icon.png")
-
-window.icon_image = icon_image
-
-window.update()
-
-print(pygame.display.Info())
+current_scene = game.get_scene()
 
 while is_running:
-    for event in get_events():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_ESCAPE:
                 is_running = False
-    window.fill((255, 255, 255))
-    window.tick()
+    #Draw
+    virtual_screen.fill((255, 255, 255))
+
+    for game_object in current_scene.game_objects:
+        virtual_screen.blit(sprites[type(game_object)], (game_object.position[0], game_object.position[1]))
+    
+
+    #Window
+    screen_size = screen.get_size()
+    
+    size = (screen_size[0]/PROPORTION_X*PROPORTION_Y, screen_size[1])
+    
+    virtual_screen = pygame.transform.scale(virtual_screen, size)
+    
+    screen.blit(virtual_screen, (screen_size[0]/2-size[0]/2, screen_size[1]/2-size[1]/2))
+    
+    pygame.display.flip()
+
+
+    #FPS
     ticks += 1
+    
     delta_time = time.time()-last_time
+    
     last_time = time.time()
-    sleep_time = 1/60 - delta_time
+    
+    sleep_time = 1/FPS - delta_time
+    
     if sleep_time < 0:
         sleep_time = 0
+        
     time.sleep(sleep_time)
+
+
 pygame.quit()
