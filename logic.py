78bc731358810f4a6708224ac397a30f4b2665ca
julfed7 <87260594+julfed7@users.noreply.github.com>
2025-01-28 06:41:21 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,4 +1,3 @@
-import utils
 import pygame
 
 
@@ -33,26 +32,58 @@ class Scene:
     def __init__(self, name):
         self.name = name
         
-        self.game_objects = pygame.sprite.Group()
+        self.game_objects = {
+        	"Abstract Game Objects": [],
+        	"Game Objects Normal Render": pygame.sprite.Group(),
+        	"Game Objects Not Normal Render": []
+        }
         
-    def add_game_object(self, new_game_object):
-    	self.game_objects.add(new_game_object)
+    def add_game_object(self, new_game_object, category):
+    	if category == "Abstract Game Objects":
+    		self.game_objects["Abstract Game Objects"].append(new_game_object)
+    	elif category == "Game Objects Normal Render":
+    		self.game_objects["Game Objects Normal Render"].add(new_game_object)
+    	elif category == "Game Objects Not Normal Render":
+    		self.game_objects["Game Objects Not Normal Render"].append(new_game_object)
     	
     def remove_game_object(self, old_game_object):
-    	self.game_objects.remove(old_game_object)
+    	abstract_game_objects_count = len(self.game_objects["Abstract Game Objects"])
+    	
+    	game_objects_normal_render_count = len(self.game_objects["Game Objects Normal Render"])
+    	
+    	game_objects_not_normal_render_count = len(self.game_objects["Game Objects Not Normal Render"])
+    	
+    	old_game_object_index = index(list(self.game_objects.values()))
+    	
+    	if old_game_object_index <= abstract_game_objects_count:
+    		self.game_objects["Abstract Game Objects"].remove(old_game_object)
+    	elif old_game_object_index <= game_objects_normal_render_count:
+    		self.game_objects["Game Objects Normal Render"].remove(old_game_object)
+    	elif old_game_object_index <= game_objects_not_normal_render_count:
+    		self.game_objects["Game Object Not Normal Render"].remove(old_game_object)
     	
     def tick(self):
-        self.game_objects.update()
+        self.game_objects["Game Objects Normal Render"].update()
+        for game_object in self.game_objects["Game Objects Not Normal Render"]:
+        	game_object.update()
+        for game_object in self.game_objects["Abstract Game Objects"]:
+        	game_object.update()
 
 
 class Grass(pygame.sprite.Sprite):
     image = None
+    
+    camera = None
     
     def __init__(self, name, position):
         self.name = name
         self.position = position
         self._image = Grass.image
         self._rect = None
+        
+    def update(self):
+        self.rect.x = self.position[0] - Grass.camera.position[0]
+        self.rect.y = self.position[1] - Grass.camera.position[1]
         
     def setup(self):
         super().__init__()
@@ -72,9 +103,11 @@ class Grass(pygame.sprite.Sprite):
     	self._rect = new_rect
  
 class PeaShooter(pygame.sprite.Sprite):
-	__animation_ticks = [3, 4, 4, 4, 4, 3, 4, 4, 4, 4, 3, 4, 4, 4, 4, 3]
+	animation_ticks = None
 	
 	animation_frame_images = []
+	
+	camera = None
 	
 	def __init__(self, name, position):
 		self.name = name
@@ -92,6 +125,9 @@ class PeaShooter(pygame.sprite.Sprite):
 		self.__last_frame_flip_ticks = self.ticks
 		
 	def update(self):
+		self.rect.x = self.position[0] - PeaShooter.camera.position[0]
+		self.rect.y = self.position[1] - PeaShooter.camera.position[1]
+		
 		if self.ticks-self.last_frame_flip_ticks == PeaShooter.animation_ticks[self.animation_current_frame]-1:
 			if self.animation_current_frame + 1 > len(PeaShooter.animation_frame_images) - 1:
 				self.animation_current_frame = 0
@@ -110,11 +146,6 @@ class PeaShooter(pygame.sprite.Sprite):
 		self.image = PeaShooter.animation_frame_images[self.animation_current_frame]
 		
 		self.rect = self.image.get_rect(x=self.position[0], y=self.position[1])
-	
-	@classmethod
-	@property
-	def animation_ticks(cls):
-		return cls.__animation_ticks
 		
 	@property
 	def ticks(self):
@@ -156,3 +187,54 @@ class PeaShooter(pygame.sprite.Sprite):
 	def animation_current_frame(self, new_animation_current_frame):
 		self.__animation_current_frame = new_animation_current_frame
 		
+		
+class Entity(pygame.sprite.Sprite):
+	camera = None
+	
+	def __init__(self, name, animation_name, position):
+		self.name = name
+		
+		self.animation_name = animation_name
+		
+		self.position = position
+		
+	def update(self):
+		pass
+	
+	def setup(self):
+		pass
+	
+class Wall(pygame.sprite.Sprite):
+	camera = None
+	
+	def __init__(self, name, animation_name, position):
+		sslf.name = name
+		
+		self.animation_name = animation_name
+		
+		self.position = position
+		
+		self.ticks = 0
+		
+		self.last_frame_flip_ticks = self.ticks
+		
+		self.animation_ticks = None
+	
+	def update(self):
+		self.rect.x = self.position[0] - Wall.camera.position[0]
+		self.rect.y = self.position[1] - Wall.camera.position[1]
+		
+		if self.ticks-self.last_frame_flip_ticks == PeaShooter.animation_ticks[self.animation_current_frame]-1:
+						if self.animation_current_frame + 1 > len(PeaShooter.animation_frame_images) - 1:
+							self.animation_current_frame = 0
+						else:
+							self.animation_current_frame += 1
+					
+					self.last_frame_flip_ticks = self.ticks
+				
+				self.image = PeaShooter.animation_frame_images[self.animation_current_frame]
+				
+				self.ticks += 1
+	
+	def setup(self):
+		pass
