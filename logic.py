71bd4400f82862a5a4a33f0f45e67dde06006376
julfed7 <87260594+julfed7@users.noreply.github.com>
2025-02-27 23:00:10 +0900

    Update logic.py

--- logic.py
+++ logic.py
@@ -2,6 +2,7 @@ import utils
 import pygame
 import copy
 import math
+import random
 
 
 class Game:
@@ -12,16 +13,23 @@ class Game:
         
         self.screen  = None
         
-    def setup(self, screen):
+        self.changed_virtual_screen_position = None
+        
+        self.virtual_screen_size = None
+        
+        self.screen_size = None
+        
+    def setup(self, screen, virtual_screen_size):
         self.screen = screen
+        self.virtual_screen_size = virtual_screen_size
         
     def render(self):
         scene = self.get_scene()
         
         scene.draw()
         
-    def tick(self, delta_time):
-        self.get_scene().tick(delta_time)
+    def tick(self, delta_time, changed_virtual_screen_position):
+        self.get_scene().tick(delta_time, changed_virtual_screen_position)
         
     def get_scene(self):
         return list(self.scenes.values())[self.current_scene]
@@ -31,6 +39,16 @@ class Game:
         
     def remove_scene(self, scene_name):
         self.scenes.pop(scene_name)
+    
+    def get_mouse_pos(self):
+    	mouse_pos = list(pygame.mouse.get_pos())
+    	if self.screen_size[0] > self.screen_size[1]:
+    		virtual_screen_size = self.virtual_screen_size[0]
+    	else:
+    		virtual_screen = (self.virtual_screen_size[1], self.virtual_screen_size[0])
+    	mouse_pos[0] = (mouse_pos[0]-self.changed_virtual_screen_position[0])/(self.screen_size[0]-self.changed_virtual_screen_position[0]*2)*self.virtual_screen_size[0]
+    	mouse_pos[1] = (mouse_pos[1]-self.changed_virtual_screen_position[1])/(self.screen_size[1]-self.changed_virtual_screen_position[1]*2)*self.virtual_screen_size[1]
+    	return mouse_pos
         
     @property
     def scenes(self):
@@ -52,18 +70,20 @@ class Scene:
         self.game = None
         
     def add_game_object(self, new_game_object):
-    	self.game_objects.add(new_game_object)
-    	
+    	self.game_objects.update({new_game_object.name:new_game_object})
+    	sorted_keys = sorted(self.game_objects)
+    	self.game_objects = {key:self.game_objects[key] for key in sorted_keys}
     def remove_game_object(self, old_game_object):
-    	seld.game_objects.remove(old_game_object)
+    	self.game_objects.pop(old_game_object.name)
     
     def draw(self):
     	for game_object in self.game_objects.values():
     		game_object.draw(self.game.screen)
+
     
-    def tick(self, delta_time):
+    def tick(self, delta_time, changed_virtual_screen_position):
         for game_object in self.game_objects.values():
-        	game_object.update(delta_time)
+        	game_object.update(delta_time, changed_virtual_screen_position)
 
 
 class GameObject(pygame.sprite.Sprite):	
@@ -86,7 +106,7 @@ class GameObject(pygame.sprite.Sprite):
 	def draw(self, screen):
 		screen.blit(self.image, self.rect)
 	
-	def update(self, delta_time):
+	def update(self, delta_time, changed_virtual_screen_position):
 		self.rect.x = self.position[0] - self.camera.position[0]
 		self.rect.y = self.position[1] - self.camera.position[1]
 			
@@ -121,11 +141,13 @@ class GameObject(pygame.sprite.Sprite):
 				
 				frame_image = pygame.image.load(utils.path(frame_path))
 				
+				frame_image.convert_alpha()
+				
 				self.animation_frame_images[animation_state].append(frame_image)
 				
 		self.image = self.animation_frame_images[self.state][self.animation_current_frame]
 				
-		self.rect = self.image.get_rect()
+		self.rect = self.image.get_rect(x=self.position[0], y=self.position[1])
 
 
 class Entity(GameObject):
@@ -133,7 +155,13 @@ class Entity(GameObject):
 
 
 class Wall(GameObject):
-	pass
+	def update(self, delta_time, changed_virtual_screen_position):
+		mouse_pos = self.scene.game.get_mouse_pos()
+		
+		if self.rect.collidepoint(mouse_pos):
+			self.position[0] = random.randint(0, 1316)
+			self.position[1] = random.randint(0, 718)
+		super().update(delta_time, changed_virtual_screen_position)
 
 
 class Camera(GameObject):
@@ -159,13 +187,15 @@ class TileMap(GameObject):
 	def setup(self):
 		super().setup()
 		
-		tilemap_info = type(self).config_tilemaps["TileMaps"][self.tilemap_name]
+		cls = type(self)
 		
-		tiles_info = type(self).config_tiles["Tiles"]
+		tilemap_info = cls.config_tilemaps["TileMaps"][self.tilemap_name]
+		
+		tiles_info = cls.config_tiles["Tiles"]
+		
+		register_objects_info = cls.register_objects_info["Register_objects"]
 		
 		self.tilemap_size = int(math.sqrt(len(tilemap_info)))
-		
-		cls = type(self)
 		
 		self.map_of_chunks_size = self.tilemap_size//cls.chunk_size
 		
@@ -180,10 +210,37 @@ class TileMap(GameObject):
 			for i in range(cls.chunk_size):
 				chunk_x = chunk_id%self.map_of_chunks_size
 				chunk_y = chunk_id//self.map_of_chunks_size
-				tiles_ids.append(tilemap_info[chunk_x*cls.chunk_size+chunk_y*cls.chunk_size*self.tilemap_size+i*cls.chunk_size*self.tilemap_size:chunk_x*cls.chunk_size+chunk_y*cls.chunk_size*self.tilemap_size+i*cls.chunk_size*self.tilemap_size+cls.chunk_size])
+				tiles_ids += (tilemap_info[chunk_x*cls.chunk_size+chunk_y*cls.chunk_size*self.tilemap_size+i*cls.chunk_size*self.tilemap_size:chunk_x*cls.chunk_size+chunk_y*cls.chunk_size*self.tilemap_size+i*cls.chunk_size*self.tilemap_size+cls.chunk_size])
 			
-			for chunk_tiles_ids in tiles_ids:
-				for tile_index, tile_id in enumerate(chunk_tiles_ids):
+			for tile_index, tile_id in enumerate(tiles_ids):
+				if tile_id not in cls.tiles_images:
+					x = tile_index%cls.chunk_size*cls.tile_size+chunk_id%self.map_of_chunks_size*cls.chunk_size*cls.tile_size
+					y = tile_index//cls.chunk_size*cls.tile_size+chunk_id//self.map_of_chunks_size*cls.chunk_size*cls.tile_size
+					
+					type_ = tiles_info[str(tile_id)]
+						
+					arguments = copy.copy(register_objects_info[type_])
+						
+					arguments.pop("Type")
+						
+					arguments.update({"Name":self.tilemap_name+str(chunk_id)+str(tile_index)})
+						
+					arguments.update({"Position":[x, y]})
+						
+					game_object = copy.copy(cls.game_object_types[type_])
+						
+					for argument_name in arguments:
+						if hasattr(game_object, argument_name.lower()):
+							setattr(game_object, argument_name.lower(), arguments[argument_name])
+								
+					game_object.camera = self.camera
+					
+					game_object.scene = self.scene
+								
+					game_object.setup()
+						
+					self.scene.add_game_object(game_object)
+				else:
 					x = tile_index%cls.chunk_size*cls.tile_size
 					y = tile_index//cls.chunk_size*cls.tile_size
 					chunk_image.blit(cls.tiles_images[tile_id], [x, y])
@@ -197,129 +254,5 @@ class TileMap(GameObject):
 				chunk_y = chunk_id//self.map_of_chunks_size*cls.chunk_size*cls.tile_size
 				screen.blit(chunk_image, [chunk_x, chunk_y])
 		
-	def update(self, delta_time):
-		super().update(delta_time)
-
-
-
-
-
-class Grass(pygame.sprite.Sprite):
-    image = None
-    
-    camera = None
-    
-    def __init__(self, name, position):
-        self.name = name
-        self.position = position
-        self._image = Grass.image
-        self._rect = None
-        
-    def update(self):
-        self.rect.x = self.position[0] - Grass.camera.position[0]
-        self.rect.y = self.position[1] - Grass.camera.position[1]
-        
-    def setup(self):
-        super().__init__()
-        
-        self.rect = self.image.get_rect(x=self.position[0], y=self.position[1])
-        	
-    @property
-    def image(self):
-        return self._image
-        
-    @property
-    def rect(self):
-    	return self._rect
-    
-    @rect.setter
-    def rect(self, new_rect):
-    	self._rect = new_rect
- 
- 
- 
- 
-class PeaShooter(pygame.sprite.Sprite):
-	animation_ticks = None
-	
-	animation_frame_images = []
-	
-	camera = None
-	
-	def __init__(self, name, position):
-		self.name = name
-		
-		self.position = position
-		
-		self.__ticks = 0
-		
-		self._image = None
-		
-		self._rect = None
-		
-		self.__animation_current_frame = 0
-		
-		self.__last_frame_flip_ticks = self.ticks
-		
-	def update(self):
-		self.rect.x = self.position[0] - PeaShooter.camera.position[0]
-		self.rect.y = self.position[1] - PeaShooter.camera.position[1]
-		
-		if self.ticks-self.last_frame_flip_ticks == PeaShooter.animation_ticks[self.animation_current_frame]-1:
-			if self.animation_current_frame + 1 > len(PeaShooter.animation_frame_images) - 1:
-				self.animation_current_frame = 0
-			else:
-				self.animation_current_frame += 1
-			
-			self.last_frame_flip_ticks = self.ticks
-		
-		self.image = PeaShooter.animation_frame_images[self.animation_current_frame]
-		
-		self.ticks += 1
-		
-	def setup(self):
-		super().__init__()
-		
-		self.image = PeaShooter.animation_frame_images[self.animation_current_frame]
-		
-		self.rect = self.image.get_rect(x=self.position[0], y=self.position[1])
-		
-	@property
-	def ticks(self):
-		return self.__ticks
-		
-	@ticks.setter
-	def ticks(self, new_ticks):
-		self.__ticks = new_ticks
-		
-	@property
-	def last_frame_flip_ticks(self):
-		return self.__last_frame_flip_ticks
-		
-	@last_frame_flip_ticks.setter
-	def last_frame_flip_ticks(self, new_ticks):
-		self.__last_frame_flip_ticks = new_ticks
-		
-	@property
-	def image(self):
-		return self._image
-	
-	@image.setter
-	def image(self, new_image):
-		self._image = new_image
-		
-	@property
-	def rect(self):
-		return self._rect
-		
-	@rect.setter
-	def rect(self, new_rect):
-		self._rect = new_rect
-		
-	@property
-	def animation_current_frame(self):
-		return self.__animation_current_frame
-		
-	@animation_current_frame.setter
-	def animation_current_frame(self, new_animation_current_frame):
-		self.__animation_current_frame = new_animation_current_frame
+	def update(self, delta_time, changed_virtual_screen_position):
+		super().update(delta_time, changed_virtual_screen_position)
